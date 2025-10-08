c2b454341cd312f0dded7cfde5b5094fc87ad8a8
shofiyyah24si <shofiyyah24si@mahasiswa.pcr.ac.id>
2025-10-08 11:09:01 +0700

    Pertemuan5_shofi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,8 +10,9 @@ Route::get('/ketua', function () {
     return view('ketua');
 });
 
-
 Route::get('/angggota', function () {
     return view('anggota');
 });
 
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
